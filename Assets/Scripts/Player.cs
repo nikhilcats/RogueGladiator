dcff2def64f05f6200864208cc73a44f5f839996
nikkydevs <nikkydevs@gmail.com>
2022-12-10 18:35:07 -0500

    Communicate with animator

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,22 +3,41 @@ using System.Collections;
 using System;
 using TMPro;
 
+// We need to make some assumptions about what direction the 
+// character spawns in. Currenty assuming always spawns "down."
+
 public class Player: MovingObject
 {
     private float moveSpeed;
     private int health = 100;
     public TextMeshProUGUI healthUIText;
+    private Vector3 localScale;
+    public Animator animator;
+
 
     protected override void Start()
     {
         moveSpeed = 5f;
         updateHealthText();
         base.Start();
+        localScale = transform.localScale;
     }
 
     protected override void Update()
     {
         base.Update();
+
+        animator.SetFloat("Horizontal", velocity.x);
+        animator.SetFloat("Vertical", velocity.y);
+        animator.SetFloat("Speed", velocity.sqrMagnitude);  //might change
+
+        if (velocity.x != 0 || velocity.y != 0)
+        {
+            animator.SetFloat("LastX", velocity.x);
+            animator.SetFloat("LastY", velocity.y);
+        }
+
+        velocity.Normalize();
         velocity *= moveSpeed;
     }
 
