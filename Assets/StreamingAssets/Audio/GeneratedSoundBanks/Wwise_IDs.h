9e53bfc3d5ffa0badf85adc0a8feab70aff9c048
Blazingrise <tribeofxd@gmail.com>
2022-12-10 23:48:34 -0800

    Wwise folder implemented in

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,57 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_ARENA_CLEAR = 893465313U;
+        static const AkUniqueID PLAY_ARENA_MUSIC = 2215072691U;
+        static const AkUniqueID PLAY_DEATH = 1172822028U;
+        static const AkUniqueID PLAY_ENTER_PORTAL = 3691806113U;
+        static const AkUniqueID PLAY_GLADIATOR_ARROW = 4080083879U;
+        static const AkUniqueID PLAY_GLADIATOR_ATTACK = 3932093680U;
+        static const AkUniqueID PLAY_GLADIATOR_BLUE_SLIME_ATTK = 1506258188U;
+        static const AkUniqueID PLAY_GLADIATOR_BLUE_SLIME_WALK = 3067123935U;
+        static const AkUniqueID PLAY_GLADIATOR_BOULDER = 2273580211U;
+        static const AkUniqueID PLAY_GLADIATOR_GREEN_SLIME_WALK = 1806245776U;
+        static const AkUniqueID PLAY_GLADIATOR_PORTAL_2 = 3628850341U;
+        static const AkUniqueID PLAY_GLADIATOR_RED_SLIME_ATTK = 1685971035U;
+        static const AkUniqueID PLAY_GLADIATOR_RED_SLIME_WALK = 3159234652U;
+        static const AkUniqueID PLAY_GLADIATOR_SLIME_DEATH = 778926299U;
+        static const AkUniqueID PLAY_GLADIATOR_SPIKES = 1927327609U;
+        static const AkUniqueID PLAY_GLADIATOR_TAKE_DMG = 2970769516U;
+        static const AkUniqueID PLAY_LV_UP = 1445801392U;
+        static const AkUniqueID PLAY_MAIN_MENU_THEME_FINAL = 2415155446U;
+        static const AkUniqueID PLAY_UI = 2044747472U;
+        static const AkUniqueID PLAY_WIZARD_APPEARS = 1535286580U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
